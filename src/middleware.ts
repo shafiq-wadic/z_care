969e6feeb86162596f454c1a77bc71cee7d52662
Muhammad Shafiq <muhammad.shafiq@wadic.dev>
2025-07-22 21:42:51 +0500

    updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,4 +3,5 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     const req = request.json();
+    console.log("req", req);
 }
